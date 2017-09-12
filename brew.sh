fc60c96755c052c8e557cedef9b5b7845e6f6c97
Stijn Huyberechts <stijn.huyberechts@gmail.com>
2017-09-12 14:30:10 +0200

    Cleanup packages

--- brew.sh
+++ brew.sh
@@ -11,7 +11,6 @@ brew upgrade
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-
 # Install some other useful utilities like `sponge`.
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
@@ -20,7 +19,7 @@ brew install findutils
 brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`. => implemented below
+# running `chsh`.
 brew install bash
 brew install bash-completion2
 
@@ -35,31 +34,24 @@ brew install wget --with-iri
 
 # Install more recent versions of some OS X tools.
 brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
+brew install grep
+brew install openssh
+brew install screen
 brew install homebrew/php/php70 --with-gmp
 
 # Install other useful binaries.
 brew install ack
 brew install dark-mode
-#brew install exiv2
 brew install git
 brew install git-lfs
 brew install git-flow
 brew install imagemagick --with-webp
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
 brew install rename
-brew install rhino
 brew install speedtest_cli
 brew install ssh-copy-id
 brew install testssl
 brew install tree
 brew install webkit2png
-#brew install zopfli
 brew install fzf
 brew install htop
 brew install php-cs-fixer
